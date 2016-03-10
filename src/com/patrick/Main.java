1f9b200f1799d6ff210f7bc0efaf1a51736758e2
Patrick <ox6185ys@go.minneapolis.edu>
2016-03-09 19:35:01 -0600

    Lake Running Log

--- src/com/patrick/Main.java
+++ src/com/patrick/Main.java
@@ -3,21 +3,65 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+//        System.out.println("Lake Running Log");
+        HashMap mapOfTimes = new HashMap();//Map of lakes (Strings) to lists of mapOfTimes.
+
+
+        ArrayList listOfTimesCalhoun = new ArrayList();
+        listOfTimesCalhoun.add(43.32);
+        listOfTimesCalhoun.add(45.15);
+        mapOfTimes.put("Calhoun", listOfTimesCalhoun);
+
+        ArrayList listOfTimesHarriet = new ArrayList();
+        listOfTimesHarriet.add(49.34);
+        listOfTimesHarriet.add(44.43);
+        listOfTimesHarriet.add(46.22);
+        mapOfTimes.put("Harriet",listOfTimesHarriet);
+
+        ArrayList listOfTimesComo = new ArrayList();
+        listOfTimesComo.add(32.11);
+        listOfTimesComo.add(28.14);
+        mapOfTimes.put("Como", listOfTimesComo);
 
         Scanner scanner = new Scanner(System.in);
         String inputLake = scanner.next();//Lake Name.
         Double inputLakeTime = scanner.nextDouble();//Running time.
 
-        HashMap times = new HashMap();//Map of lakes to lists of times.
 
-        boolean doesContainLake = times.containsValue(inputLake);
-        if (!doesContainLake){
+        boolean doesContainLake = mapOfTimes.containsKey(inputLake);
+        if (!doesContainLake) {
             ArrayList listOfTimes = new ArrayList();
             listOfTimes.add(inputLakeTime);
-            times.put(inputLake,listOfTimes);
+            mapOfTimes.put(inputLake, listOfTimes);
+        } else {
+            List timesList = (List)mapOfTimes.get(inputLake);
+            timesList.add(inputLakeTime);
+
         }
-        else{
-            times.get(inputLake);
+
+
+        for (Object o : mapOfTimes.keySet()) {
+            String nameOfLake = (String) o;
+            List lakeTimes = (List) mapOfTimes.get(nameOfLake);
+            Double lowestTime = getQuickestTime(lakeTimes);
+
+            System.out.println(nameOfLake +", " + lowestTime);
+            //System.out.println(lowestTime);
+
         }
-       }
+
     }
+    public static Double getQuickestTime(List lakeTimes){
+        Double quickestTime = Double.POSITIVE_INFINITY;
+        for (Object o : lakeTimes) {
+            Double time = (Double) o;
+            if (time < quickestTime)
+                quickestTime = time;
+           // System.out.println(o);
+        }
+        return quickestTime;
+//if time < quickestTime
+    }
+}
+
+
